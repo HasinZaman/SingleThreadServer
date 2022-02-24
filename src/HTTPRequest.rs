38c509ae11d1c4a53405775a69bedac01a3d2a8a
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-02-24 14:29:07 -0500

    Created HTTPBody mod
    Features
     - HTTPBody handles enums and structs assoicated with body
      - ContentType enum defines the general content type of body
      - Value mod define the specific content type for general content types

--- src/HTTPRequest.rs
+++ src/HTTPRequest.rs
@@ -29,6 +29,208 @@ pub enum ParserError {
     NotImplemented,
 }
 
+pub mod HTTPBody {
+    trait VariantName {
+        fn get_variant(&self) -> String;
+    }
+
+    //https://www.geeksforgeeks.org/http-headers-content-type/
+    pub enum ContentType {
+        Application {value : Value::Application},
+        Audio {value : Value::Audio},
+        Image {value : Value::Image},
+        Multipart {value : Value::Multipart},
+        Text {value : Value::Text},
+        Video {value : Value::Video},
+    }
+
+    pub mod Value{
+        pub enum Application {
+            EDI_X12,
+            EDIFACT,
+            javascript,
+            octet_stream,
+            ogg,
+            pdf,
+            xhtml_xml,
+            x_shockwave_flash,
+            json,
+            ld_json,
+            xml,
+            zip,
+            x_www_form_urlencoded,
+        }
+        impl super::VariantName for Application {
+            fn get_variant(&self) -> String{
+                match &self {
+                    Application::EDI_X12 => return String::from("EDI-X12"),
+                    Application::EDIFACT => return String::from("EDIFACT"),
+                    Application::javascript => return String::from("javascript"),
+                    Application::octet_stream => return String::from("octet-stream"),
+                    Application::ogg => return String::from("ogg"),
+                    Application::pdf => return String::from("pdf"),
+                    Application::xhtml_xml => return String::from("xhtml+xml"),
+                    Application::x_shockwave_flash => return String::from("x-shockwave-flash"),
+                    Application::json => return String::from("json"),
+                    Application::ld_json => return String::from("ld+json"),
+                    Application::xml => return String::from("xml"),
+                    Application::zip => return String::from("zip"),
+                    Application::x_www_form_urlencoded => return String::from("x-www-form-urlencoded"),
+                    _ => panic!("Invalid variant type"),
+                }
+            }
+        }
+
+        pub enum Audio {
+            mpeg,
+            x_ms_wma,
+            vnd_rn_realaudio,
+            x_wav,
+        }
+        impl super::VariantName for Audio {
+            fn get_variant(&self) -> String{
+                match &self {
+                    Audio::mpeg => return String::from("mpeg"),
+                    Audio::x_ms_wma => return String::from("x-ms-wma"),
+                    Audio::vnd_rn_realaudio => return String::from("vnd.rn-realaudio"),
+                    Audio::x_wav => return String::from("x-wav"),
+                    _ => panic!("Invalid variant type"),
+                }
+            }
+        }
+
+        pub enum Image {
+            gif,
+            jpeg,
+            png,
+            tiff,
+            vnd_microsoft_icon,
+            x_icon,
+            vnd_djvu,
+            svg_xml,
+        }
+        impl super::VariantName for Image {
+            fn get_variant(&self) -> String{
+                match &self {
+                    Image::gif => return String::from("gif"),
+                    Image::jpeg => return String::from("jpeg"),
+                    Image::png => return String::from("png"),
+                    Image::tiff => return String::from("tiff"),
+                    Image::vnd_microsoft_icon => return String::from("vnd.microsoft.icon"),
+                    Image::x_icon => return String::from("x-icon"),
+                    Image::vnd_djvu => return String::from("vnd.djvu"),
+                    Image::svg_xml => return String::from("svg+xml"),
+                    _ => panic!("Invalid variant type"),
+                }
+            }
+        }
+
+        pub enum Multipart {
+            mixed,
+            alternative,
+            related,
+            form_data {boundary : String},
+        }
+        impl super::VariantName for Multipart {
+            fn get_variant(&self) -> String{
+                match &self {
+                    Multipart::mixed => String::from("mixed"),
+                    Multipart::alternative => String::from("alternative"),
+                    Multipart::related => String::from("related"),
+                    Multipart::form_data { boundary } => String::from("form-data"),
+                    _ => panic!("Invalid variant type"),
+                }
+            }
+        }
+
+        pub enum Text {
+            css,
+            csv,
+            html,
+            javascript,
+            plain,
+            xml,
+        }
+        impl super::VariantName for Text {
+            fn get_variant(&self) -> String{
+                match &self {
+                    Text::css => String::from("css"),
+                    Text::csv => String::from("csv"),
+                    Text::html => String::from("html"),
+                    Text::javascript => String::from("javascript"),
+                    Text::plain => String::from("plain"),
+                    Text::xml => String::from("xml"),
+                    _ => panic!("Invalid variant type"),
+                }
+            }
+        }
+
+        pub enum Video {
+            mpeg,
+            mp4,
+            quicktime,
+            x_ms_wmv,
+            x_msvideo,
+            x_flv,
+            webm,
+        }
+        impl super::VariantName for Video {
+            fn get_variant(&self) -> String{
+                match &self {
+                    Video::mpeg => String::from("mpeg"),
+                    Video::mp4 => String::from("mp4"),
+                    Video::quicktime => String::from("quicktime"),
+                    Video::x_ms_wmv => String::from("x-ms-wmv"),
+                    Video::x_msvideo => String::from("x-msvideo"),
+                    Video::x_flv => String::from("x-flv"),
+                    Video::webm => String::from("webm"),
+                    _ => panic!("Invalid variant type"),
+                }
+            }
+        }
+
+    }
+    impl VariantName for ContentType {
+        fn get_variant(&self) -> String{
+            match &self {
+                ContentType::Application { value } => return String::from("application"),
+                ContentType::Audio { value } => return String::from("audio"),
+                ContentType::Image { value } => return String::from("image"),
+                ContentType::Multipart { value } => return String::from("multipart"),
+                ContentType::Text { value } => return String::from("text"),
+                ContentType::Video { value } => return String::from("video"),
+                _ => panic!("Invalid variant type"),
+            }
+        }
+    }
+    impl ToString for ContentType {
+        fn to_string(&self) -> String {
+            match &self {
+                ContentType::Application { value }  => {
+                    return format!("{}/{}", &self.get_variant(), value.get_variant())
+                },
+                ContentType::Audio { value }   => {
+                    return format!("{}/{}", &self.get_variant(), value.get_variant())
+                },
+                 ContentType::Image { value }   => {
+                    return format!("{}/{}", &self.get_variant(), value.get_variant())
+                },
+                 ContentType::Multipart { value }   => {
+                    return format!("{}/{}", &self.get_variant(), value.get_variant())
+                },
+                 ContentType::Text { value }   => {
+                    return format!("{}/{}", &self.get_variant(), value.get_variant())
+                },
+                 ContentType::Video { value } => {
+                    return format!("{}/{}", &self.get_variant(), value.get_variant())
+                },
+                _ => panic!("Not implemented variant"),
+            }
+            
+        }
+    }
+}
+
 impl Method {
     pub fn new(request_data : [u8; 1024]) -> Result<Method, ParserError> {
         let request : String = String::from_utf8_lossy(&request_data[..]).to_string();

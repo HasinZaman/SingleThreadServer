29f5d7c8fb9ce1e36d72523fe2a815b0b0480a9c
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-02-27 13:45:38 -0500

    Replaced all parsing cases with NotImplemented error

--- src/HTTPRequest.rs
+++ src/HTTPRequest.rs
@@ -424,15 +424,15 @@ impl Method {
                 let method : String = method.to_uppercase();
 
                 match method.as_str() {
-                    "GET"       => return Method::new_GET(request),
-                    "HEAD"      => return Method::new_HEAD(request),
-                    "POST"      => return Method::new_POST(request),
-                    "PUT"       => return Method::new_PUT(request),
-                    "DELETE"    => return Method::new_DELETE(request),
-                    "CONNECT"   => return Method::new_CONNECT(request),
-                    "OPTIONS"   => return Method::new_OPTIONS(request),
-                    "TRACE"     => return Method::new_TRACE(request),
-                    "PATCH"     => return Method::new_PATCH(request),
+                    "GET"       => return Result::Err(ParserError::NotImplemented),
+                    "HEAD"      => return Result::Err(ParserError::NotImplemented),
+                    "POST"      => return Result::Err(ParserError::NotImplemented),
+                    "PUT"       => return Result::Err(ParserError::NotImplemented),
+                    "DELETE"    => return Result::Err(ParserError::NotImplemented),
+                    "CONNECT"   => return Result::Err(ParserError::NotImplemented),
+                    "OPTIONS"   => return Result::Err(ParserError::NotImplemented),
+                    "TRACE"     => return Result::Err(ParserError::NotImplemented),
+                    "PATCH"     => return Result::Err(ParserError::NotImplemented),
                     _           => return Result::Err(ParserError::InvalidMethod),
                 }
             },
@@ -440,74 +440,6 @@ impl Method {
                 return Result::Err(ParserError::InvalidMethod);
             },
         }
-    }
-
-    fn new_GET(request : Vec<&str>) -> Result<Method, ParserError> {
-        let start_line : Vec<&str> = request[0].clone().split_whitespace().collect();
-
-        if start_line.len() < 3 {
-            return Result::Err(ParserError::InvalidMethod);
-        }
-
-        let file_name : &str = start_line[1];
-
-        Result::Ok(Method::GET{ file : file_name.to_string()})   
-    }
-
-    fn new_HEAD(request : Vec<&str>) -> Result<Method, ParserError> {
-        let start_line : Vec<&str> = request[0].clone().split_whitespace().collect();
-
-        if start_line.len() < 3 {
-            return Result::Err(ParserError::InvalidMethod);
-        }
-
-        let file_name : &str = start_line[1];
-
-        Result::Ok(Method::HEAD{ file : file_name.to_string()})
-    }
-
-    fn new_POST(request : Vec<&str>) -> Result<Method, ParserError> {
-        Result::Err(ParserError::NotImplemented)
-    }
-
-    fn new_PUT(request : Vec<&str>) -> Result<Method, ParserError> {
-        Result::Err(ParserError::NotImplemented)
-    }
-
-    fn new_DELETE(request : Vec<&str>) -> Result<Method, ParserError> {
-        Result::Err(ParserError::NotImplemented)
-    }
-
-    fn new_CONNECT(request : Vec<&str>) -> Result<Method, ParserError> {
-        let start_line : Vec<&str> = request[0].clone().split_whitespace().collect();
-
-        if start_line.len() < 3 {
-            return Result::Err(ParserError::InvalidMethod);
-        }
-
-        let URL_name : &str = start_line[1];
-
-        Result::Ok(Method::CONNECT{ URL : URL_name.to_string()})
-    }
-
-    fn new_OPTIONS(request : Vec<&str>) -> Result<Method, ParserError> {
-        let start_line : Vec<&str> = request[0].clone().split_whitespace().collect();
-
-        if start_line.len() < 3 {
-            return Result::Err(ParserError::InvalidMethod);
-        }
-
-        let URL_name : &str = start_line[1];
-
-        Result::Ok(Method::OPTIONS{ URL : URL_name.to_string()})
-    }
-
-    fn new_TRACE(request : Vec<&str>) -> Result<Method, ParserError> {
-        Result::Err(ParserError::NotImplemented)
-    }
-
-    fn new_PATCH(request : Vec<&str>) -> Result<Method, ParserError> {
-        Result::Err(ParserError::NotImplemented)
     }
 
     fn get_type<'a>(request: &'a str) -> Option<&'a str> {

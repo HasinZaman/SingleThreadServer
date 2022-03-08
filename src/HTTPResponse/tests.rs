5a3465bce15c15ea5909c9b3df8976966c6b0c0e
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-08 18:04:58 -0500

    Created to_string tests for response.rs

--- src/HTTPResponse/tests.rs
+++ src/HTTPResponse/tests.rs
@@ -19,3 +19,35 @@ mod response_status_code{
         assert_eq!(response_status_code.to_string(), "201 Created");
     }
 }
+
+mod response{
+    use super::super::Response::Response;
+    use super::super::ResponseStatusCode::ResponseStatusCode;
+    use crate::HTTPRequest::HTTPBody::{Body, ContentType, Value};
+    //use crate::HTTPRequest::HTTPBody::*;
+
+    #[test]
+    fn no_body_test(){
+        let response = Response {
+            status : ResponseStatusCode::Ok,
+            body : Option::None
+        };
+
+        assert_eq!(response.to_string(), "200 Ok\r");
+    }
+
+    #[test]
+    fn body_test(){
+        let response = Response {
+            status : ResponseStatusCode::Ok,
+            body : Option::Some(
+                Body {
+                    content_type : ContentType::Text { value : Value::Text::html },
+                    content: String::from("<p>New File</p>")
+                }
+            )
+        };
+
+        assert_eq!(response.to_string(), "200 Ok\r\nContent-Length: 15\r\nContent-Type: text/html\r\n\n<p>New File</p>");
+    }
+}

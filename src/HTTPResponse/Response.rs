49bab69fc8c30ffe1709d4e3e0abc9a0fc4af65a
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-08 18:05:41 -0500

    implemented Body check & parsing to to_string

--- src/HTTPResponse/Response.rs
+++ src/HTTPResponse/Response.rs
@@ -2,13 +2,29 @@ use crate::HTTPRequest::HTTPBody::Body;
 use super::ResponseStatusCode::*;
 
 pub struct Response {
-    status : ResponseStatusCode,
-    body : Option<Body>
+    pub status : ResponseStatusCode,
+    pub body : Option<Body>
 }
 
 impl ToString for Response {
     fn to_string(&self) -> String {
         let mut tmp : String = format!("{}\r", self.status.to_string());
+
+        match &self.body {
+            Some(body) => {
+                tmp.push_str("\n");
+
+                let content_length = format!("Content-Length: {}\r\n", body.content.len());
+                tmp.push_str(&content_length);
+
+                let content_type = format!("Content-Type: {}\r\n", body.content_type.to_string());
+                tmp.push_str(&content_type);
+
+                let content = format!("\n{}", &(body.content));
+                tmp.push_str(&content);
+            },
+            None => {},
+        }
 
         tmp
     }

79f7fdce3ee0a0b6cda05d1ce167b64e90a39ae5
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-09 11:57:44 -0500

    Added default_get_logic closure generator

--- src/Server.rs
+++ src/Server.rs
@@ -1,5 +1,6 @@
-use crate::HTTPRequest::Method::Method;
+use crate::HTTPRequest::{Method::Method, HTTPBody};
 use crate::HTTPResponse::{Response::Response, ResponseStatusCode::ResponseStatusCode};
+use crate::FileReader;
 
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -19,13 +20,140 @@ pub struct MethodLogic {
 }
 
 impl MethodLogic {
-    pub fn default_logic() -> Box<dyn Fn(Method)->Response> {
+    pub fn default_not_allowed_logic() -> Box<dyn Fn(Method)->Response> {
         Box::new(
             |_request : Method | {
                 Response {
                     status : ResponseStatusCode::MethodNotAllowed,
                     body : Option::None
                 }
+            }
+        )
+    }
+
+    pub fn default_get_logic() -> Box<dyn Fn(Method)->Response> {
+        Box::new(
+            |request : Method | {
+                match request {
+                    Method::GET{ file } => {
+                        let path_buf = FileReader::parse(&file);
+
+                        let file_name = match &path_buf.file_name(){
+                            Some(name) => name.to_str().unwrap(),
+                            None => panic!("FileReader::parse does not return file name. {:?}", path_buf),
+                        };
+
+                        let body = FileReader::get_file_content(&path_buf).unwrap();
+
+                        match file_name {
+                            "404.html" => {
+                                return Response {
+                                    status : ResponseStatusCode::NotFound,
+                                    body : Option::Some(
+                                        HTTPBody::Body {
+                                            content_type: HTTPBody::ContentType::Text{
+                                                value: HTTPBody::value::Text::html
+                                            },
+                                            content: body,
+                                        }
+                                    )
+                                } 
+                            },
+                            _ => {
+                                let content_type = path_buf.extension().unwrap();
+                                let content_type = content_type.to_str().unwrap();
+
+                                let content_type : HTTPBody::ContentType = match content_type {
+                                    "gif" => {
+                                        HTTPBody::ContentType::Image{
+                                            value: HTTPBody::value::Image::gif
+                                        }
+                                    },
+                                    "jpg" | "jpeg" | "jpe" | "jfif" => {
+                                        HTTPBody::ContentType::Image{
+                                            value: HTTPBody::value::Image::jpeg
+                                        }
+                                    },
+                                    "png" => {
+                                        HTTPBody::ContentType::Image{
+                                            value: HTTPBody::value::Image::png
+                                        }
+                                    },
+                                    "tif" | "tiff" => {
+                                        HTTPBody::ContentType::Image{
+                                            value: HTTPBody::value::Image::tiff
+                                        }
+                                    },
+                                    "css" => {
+                                        HTTPBody::ContentType::Text{
+                                            value: HTTPBody::value::Text::css
+                                        }
+                                    },
+                                    "csv" => {
+                                        HTTPBody::ContentType::Text{
+                                            value: HTTPBody::value::Text::csv
+                                        }
+                                    },
+                                    "html" => {
+                                        HTTPBody::ContentType::Text{
+                                            value: HTTPBody::value::Text::html
+                                        }
+                                    },
+                                    "javascript" => {
+                                        HTTPBody::ContentType::Text{
+                                            value: HTTPBody::value::Text::javascript
+                                        }
+                                    },
+                                    "xml" => {
+                                        HTTPBody::ContentType::Text{
+                                            value: HTTPBody::value::Text::xml
+                                        }
+                                    },
+                                    "mpeg" => {
+                                        HTTPBody::ContentType::Video{
+                                            value: HTTPBody::value::Video::mpeg
+                                        }
+                                    },
+                                    "mp4" => {
+                                        HTTPBody::ContentType::Video{
+                                            value: HTTPBody::value::Video::mp4
+                                        }
+                                    },
+                                    "webm" => {
+                                        HTTPBody::ContentType::Video{
+                                            value: HTTPBody::value::Video::webm
+                                        }
+                                    },
+                                    _=>{
+                                        return Response {
+                                            status : ResponseStatusCode::NotFound,
+                                            body : Option::None
+                                        } 
+                                    }
+                                };
+
+
+
+                                return Response {
+                                    status : ResponseStatusCode::Ok,
+                                    body : Option::Some(
+                                        HTTPBody::Body {
+                                            content_type: content_type,
+                                            content: body,
+                                        }
+                                    )
+                                } 
+                            }
+                        }
+                    },
+                    _=> {
+                        Response {
+                            status : ResponseStatusCode::BadRequest,
+                            body : Option::None
+                        } 
+                    }
+                }
+                
             }
         )
     }

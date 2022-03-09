78dfd16d7895239f41a4af7a52b48df6226305d6
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-09 10:20:16 -0500

    removed \r

--- src/HTTPResponse/Response.rs
+++ src/HTTPResponse/Response.rs
@@ -8,16 +8,16 @@ pub struct Response {
 
 impl ToString for Response {
     fn to_string(&self) -> String {
-        let mut tmp : String = format!("{}\r", self.status.to_string());
+        let mut tmp : String = format!("{}", self.status.to_string());
 
         match &self.body {
             Some(body) => {
                 tmp.push_str("\n");
 
-                let content_length = format!("Content-Length: {}\r\n", body.content.len());
+                let content_length = format!("Content-Length: {}\n", body.content.len());
                 tmp.push_str(&content_length);
 
-                let content_type = format!("Content-Type: {}\r\n", body.content_type.to_string());
+                let content_type = format!("Content-Type: {}\n", body.content_type.to_string());
                 tmp.push_str(&content_type);
 
                 let content = format!("\n{}", &(body.content));

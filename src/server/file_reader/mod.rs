1f86e304318f93172e5bcdc82b01bc00a7f1b268
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-17 11:40:47 -0400

    Added Documentation for file_reader module

--- src/server/file_reader/mod.rs
+++ src/server/file_reader/mod.rs
@@ -1,3 +1,5 @@
+//! file_reader module is responsible for storing functions that parses file paths and retrieving file data
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::{Path, PathBuf};
@@ -5,6 +7,10 @@ use std::path::{Path, PathBuf};
 #[cfg(test)]
 mod tests;
 
+/// parse method converts a file path into a PathBuf
+/// 
+/// # Errors
+/// Option::None is returned instead of a PathBuf if file does not exist or the file has unaccessible extensions. Unaccessible extensions are defined in settings.ron for each path.
 pub fn parse(url: &str, search_folder: &str, allowed_extension: &Vec<String>) -> Option<PathBuf> {
     let url = url.trim_matches('\\').trim_matches('/');
 
@@ -37,6 +43,11 @@ pub fn parse(url: &str, search_folder: &str, allowed_extension: &Vec<String>) ->
     return Option::None;
 }
 
+/// get_file_content_string returns string content of a file
+/// 
+/// # Errors
+/// None is returned if the file does not exist
+/// None is also returned if file cannot be read as a string. Ex. an file like jpg would cause this error
 pub fn get_file_content_string(file_path: &Path) -> Option<String> {
     let mut file = match File::open(file_path) {
         Err(_err) => return Option::None,
@@ -51,6 +62,10 @@ pub fn get_file_content_string(file_path: &Path) -> Option<String> {
     }
 }
 
+/// get_file_content_bytes returns vector of bytes of file contents
+/// 
+/// # Errors
+/// None is returned if the file does not exist
 pub fn get_file_content_bytes(file_path: &Path) -> Option<Vec<u8>> {
     let mut file = match File::open(file_path) {
         Err(_err) => return Option::None,

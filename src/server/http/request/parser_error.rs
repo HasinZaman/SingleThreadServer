987ed6876b88b20e32cac5533fc3580dd21c4c4e
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-17 11:40:09 -0400

    Added Documentation for parser_error.rs

--- src/server/http/request/parser_error.rs
+++ src/server/http/request/parser_error.rs
@@ -1,3 +1,6 @@
+//! Parser_error module defines enums required to parsing errors
+
+/// ParserError enum defines variants that represent errors that can emerge through parsing of request
 #[derive(Debug)]
 pub enum ParserError {
     InvalidMethod(Option<String>),//split invalid method into more precise errors

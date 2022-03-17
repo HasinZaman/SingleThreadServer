f378ecce048eef680477a1c4a4472afbf297c3dd
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-17 09:26:04 -0400

    Added documentation to Setting.rs

--- src/server/setting.rs
+++ src/server/setting.rs
@@ -1,3 +1,5 @@
+//! Setting module handles the reading and storing of server settings
+
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -5,6 +7,7 @@ use ron::de::from_str;
 use serde::Deserialize;
 use std::collections::HashMap;
 
+/// ServerSetting is a struct that stores key information required for server start up, HTTP method handling and file retrieval
 #[derive(Debug, Deserialize)]
 pub struct ServerSetting {
     pub address: String,
@@ -12,6 +15,7 @@ pub struct ServerSetting {
     pub paths: HashMap<String, DomainPath>,
 }
 
+/// DomainPath defines the path of domain in the source directory; and the extensions that can be received through GET or HEAD requests. 
 #[derive(Debug, Deserialize)]
 pub struct DomainPath {
     pub path: String,
@@ -19,6 +23,15 @@ pub struct DomainPath {
 }
 
 impl ServerSetting {
+    /// load functions read settings.ron
+    /// 
+    /// # return
+    /// ServerSetting struct that reflect the data stored in settings.ron
+    /// 
+    /// #Examples
+    /// ```
+    /// let server_setting : ServerSetting = ServerSetting::Load();
+    /// ```
     pub fn load() -> ServerSetting {
         let mut file = match File::open("settings.ron") {
             Ok(file) => file,

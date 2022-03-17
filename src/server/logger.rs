58e46d764921a3e41498a891c86fe029bc850b00
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-17 09:47:26 -0400

    Added Documentation to logger.rs

--- src/server/logger.rs
+++ src/server/logger.rs
@@ -1,8 +1,12 @@
+//! logger module handles logging messages for debugging purposes
 use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
 use std::os::windows::prelude::FileExt;
 use std::path::Path;
 
+/// set_up method ensures there exists a "log.txt" for logging
+/// 
+/// If "log.txt" does not exist; then set_up will create a "log.txt" in the root folder
 pub fn set_up() {
     if !log_file_check() {
         if let Result::Err(err) = File::create("log.txt") {
@@ -11,6 +15,20 @@ pub fn set_up() {
     }
 }
 
+/// log method adds message to log.txt
+/// 
+/// The message is in the format
+/// ```text
+/// --- --- ---
+/// (tag)   (time_stamp)
+/// (message)
+/// --- --- ---
+/// ```
+/// 
+/// # Example
+/// ```
+/// log("Hello", "World");
+/// ```
 pub fn log(tag: &str, message: String) {
     if !log_file_check() {
         set_up();
@@ -46,6 +64,7 @@ pub fn log(tag: &str, message: String) {
     }
 }
 
+/// log_file_check checks if log.txt exists
 fn log_file_check() -> bool {
     Path::new("log.txt").exists()
 }
